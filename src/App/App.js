8b5f47d42ee8050cc30e725ea4474f90ad74ae50
Elizabeth O. Segun-Ajiboye <66387727+ElizabethdevQueen@users.noreply.github.com>
2021-04-10 17:40:52 +0100

    the main page

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,59 @@
+import { CssBaseline, makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core';
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
+import Header from '../components/Header';
+import PageHeader from '../components/PageHeader';
+import SideMenu from '../components/SideMenu';
+import Employees from '../pages/Employees/Employees';
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  },
+  overrides:{
+    MuiAppBar:{
+      root:{
+        transform:'translateZ(0)'
+      }
+    }
+  },
+  props:{
+    MuiIconButton:{
+      disableRipple:true
+    }
+  }
+})
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
+  }
+})
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      hi
-    </div>
+    <ThemeProvider theme={theme}>
+      <SideMenu />
+      <div className={classes.appMain}>
+        <Header />
+
+        <Employees />
+      </div>
+      <CssBaseline /> 
+    </ThemeProvider>
   );
 }
 
